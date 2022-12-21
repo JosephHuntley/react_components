ab4632d53672788527ae7bea0e5d733ca6913c2d
Joseph Huntley <Joseph.Huntley@outlook.com>
2022-12-21 13:00:40 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import SnowGlobe from './components/snowGlobe/SnowGlobe'
+import Nav from './components/nav/Nav'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Nav/>
+      <div className="container">
+      <SnowGlobe/>
+      </div>
+    </div>
+  )
+}
+
+export default App
